5ade3301cf47758ae5739390f885839654d05cef
martinBogdanov1 <martin.petrov1994@yahoo.com>
2022-10-21 21:11:49 +0300

    Introduce navbar with all the categories

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const productService = require('../services/product');
+const categoryService = require('../services/category');
 
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  const product = await productService.getById('5172d203ffdd81f3234d5f8a');
-  res.json(product);
-
+router.get('/', async function (req, res, next) {
+  const categories = await categoryService.getAll();
+  res.render('index.ejs', { categories });
 });
 
 module.exports = router;
