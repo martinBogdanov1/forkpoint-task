02b9b6fcdb473fe0c4430f3b971ef7c6fcef4fed
martinBogdanov1 <martin.petrov1994@yahoo.com>
2022-10-22 23:13:13 +0300

    Display products with pagination

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
 const categoryService = require('../services/category');
-
+const productService = require('../services/product');
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
+router.get('/products', async function (req, res) {
+  const page = req.query.page;
   const categories = await categoryService.getAll();
-  res.render('index.ejs', { categories });
+  const products = await productService.getProductsWithLimit({}, page);
+
+  res.render('index.ejs', { products, categories });
 });
+
+/* GET category page. */
+router.get('/:categoryId', async function (req, res) {
+  const page = req.query.page;
+  const primary_category_id = req.params.categoryId;
+  const products = await productService.getProductsWithLimit({ primary_category_id }, page);
+  const categories = await categoryService.getAll();
+
+  res.render('index.ejs', { products, categories });
+});
+
 
 module.exports = router;
