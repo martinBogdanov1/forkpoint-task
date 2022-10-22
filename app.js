bd57577fa6bc015cec64a3d264a31bb26118bfdc
martinBogdanov1 <martin.petrov1994@yahoo.com>
2022-10-22 23:01:20 +0300

    Delete unused files and imports

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var logger = require('morgan');
 var databaseConfig = require('./config/database');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 
 var app = express();
 startDb();
@@ -23,8 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
