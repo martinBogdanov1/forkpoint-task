58a5634fe6825ebb21049d33f8d39a794d145652
martinBogdanov1 <martin.petrov1994@yahoo.com>
2022-10-23 21:58:14 +0300

    Add method for fetching all filters and filter values

--- services/product.js
+++ services/product.js
@@ -10,25 +10,52 @@ async function getProductsWithLimit(filter = {}, page = 1, limit = 16) {
 
     const count = await Product.countDocuments(filter);
     return {
-        items: transferDetailsForDisplay(products),
+        items: transformDetailsForDisplay(products),
         totalPages: Math.ceil(count / limit),
         currentPage: page
     }
 }
 
-function transferDetailsForDisplay(products) {
+async function getFilters() {
+    const variationAttributes = await Product
+        .find({ variation_attributes: { $ne: null } })
+        .lean();
+
+    let attributesValues = [];
+    variationAttributes.map(attributes => {
+        attributes.variation_attributes.forEach(item => {
+            const valuesIndex = attributesValues.findIndex(x => (x.id == item.id));
+            if (valuesIndex <= -1) {
+                attributesValues.push({ id: item.id, values: [] });
+            }
+
+            attributesValues.forEach(attributeValue => {
+                item.values.forEach(itemValue => {
+                    const valueIndex = attributeValue.values.indexOf(itemValue.name);
+                    if (item.id === attributeValue.id && valueIndex == -1) {
+                        attributeValue.values.push(itemValue.name);
+                    }
+                });
+            });
+        });
+    });
+    return attributesValues;
+}
+
+function transformDetailsForDisplay(products) {
     return products.map(product => {
         return {
             name: product.name,
             price: product.price,
             price_max: product.price_max,
             price: product.price,
-            image: product.image_groups.filter(img => img.view_type == 'large')[0].images[0].link
+            image: product.image_groups.filter(img => img.view_type == 'large')[0].images[0].link,
+            filters: product.variation_attributes
         }
     });
 };
 
-
 module.exports = {
-    getProductsWithLimit
+    getProductsWithLimit,
+    getFilters
 };
