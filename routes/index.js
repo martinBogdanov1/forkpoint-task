89e521e0e5d38e3e93c239b8118bd66ac1480705
martinBogdanov1 <martin.petrov1994@yahoo.com>
2022-10-24 17:10:53 +0300

    Implement filters functionality and dynamic pagination

--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,94 @@ const categoryService = require('../services/category');
 const productService = require('../services/product');
 
 /* GET home page. */
-router.get('/products', async function (req, res) {
+router.get('/', async function (req, res) {
   const page = req.query.page;
-  const categories = await categoryService.getAll();
-  const products = await productService.getProductsWithLimit({}, page);
-  const filters = await productService.getFilters();
+  const query = Object.entries(req.query);
+  const filter = parseQuery(query);
 
-  res.render('index.ejs', { products, categories, filters });
+  const categories = await categoryService.getAll();
+  const filters = await productService.getFilters();
+  const products = await productService.getProductsWithLimit(filter, page);
+
+  const currentUrl = req.originalUrl;
+
+  const nextPage = getNextPage(currentUrl, products.currentPage);
+  const previousPage = getPreviousPage(currentUrl, products.currentPage);
+
+  res.render('index.ejs', { products, categories, filters, nextPage, previousPage });
 });
 
 /* GET category page. */
 router.get('/:categoryId', async function (req, res) {
   const page = req.query.page;
   const primary_category_id = req.params.categoryId;
-  const products = await productService.getProductsWithLimit({ primary_category_id }, page);
-  const categories = await categoryService.getAll();
+  const query = Object.entries(req.query);
+  const filter = parseQuery(query, primary_category_id);
 
-  res.render('index.ejs', { products, categories });
+  const categories = await categoryService.getAll();
+  const products = await productService.getProductsWithLimit(filter, page);
+  const filters = await productService.getFilters(primary_category_id);
+
+  const currentUrl = req.originalUrl;
+
+  const nextPage = getNextPage(currentUrl, products.currentPage);
+  const previousPage = getPreviousPage(currentUrl, products.currentPage);
+
+  res.render('index.ejs', { products, categories, filters, nextPage, previousPage });
 });
 
-router.post('/products', async function (req, res) {
-  console.log(req.body);
-  res.end();
-})
 
+function parseQuery(query, categoryId) {
+  let filter = {};
+
+  if (categoryId) {
+    filter = { primary_category_id: categoryId };
+  }
+
+  if (query.length > 0) {
+    filter['$and'] = [];
+
+    query.forEach(q => {
+      const filterId = q[0];
+      const filterValue = q[1];
+
+      if (filterId != 'page') {
+        filter.$and.push({
+          variation_attributes: {
+            $elemMatch: { id: filterId, values: { $elemMatch: { name: filterValue } } }
+          }
+        });
+      }
+    });
+  }
+
+  if (filter.$and?.length === 0) {
+    delete filter.$and;
+  }
+
+  return filter;
+}
+
+function getNextPage(currentPath, currentPage) {
+  if (currentPath.includes('page')) {
+    currentPath = currentPath.replace(`page=${currentPage}`, `page=${currentPage + 1}`);
+  } else {
+    if (currentPath.includes('?')) {
+      currentPath += `&page=${currentPage + 1}`;
+    } else {
+      currentPath += `?page=${currentPage + 1}`;
+    }
+  }
+
+  return currentPath;
+}
+
+function getPreviousPage(currentPath, currentPage) {
+  if (currentPath.includes('page') && currentPage > 1) {
+    currentPath = currentPath.replace(`page=${currentPage}`, `page=${currentPage - 1}`);
+  } 
+
+  return currentPath;
+}
 
 module.exports = router;
